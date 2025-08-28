1877d49260b00be952e094c6e13461335bce1939
Borys Ziukov <ziukoff1985@gmail.com>
2025-08-28 12:22:28 +0300

    Add general logic

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const targetStr = 'Some men interpret nine memos';
+const separator = '';
+
+function isStrPalindrome(string, separator) {
+    const targetStrWithoutSpaces = string
+        .toLowerCase()
+        .replaceAll(' ', separator);
+    const targetStrWithoutSpacesReversed = targetStrWithoutSpaces
+        .split(separator)
+        .reverse()
+        .join(separator);
+    return targetStrWithoutSpaces === targetStrWithoutSpacesReversed;
+}
+
+console.log(isStrPalindrome(targetStr, separator));
