951788976df8c32f128d653343e6076f8597f54d
Borys Ziukov <ziukoff1985@gmail.com>
2025-08-28 13:26:50 +0300

    Add regExp for normalize

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 'use strict';
 
-const string = 'Some men interpret nine memos';
-const separator = '';
+const string = 'A man, a plan, a canal: Panama';
 
-function isStrPalindrome(string, separator) {
-    const stringWithoutSpaces = string.toLowerCase().replaceAll(' ', separator);
-    const stringWithoutSpacesReversed = stringWithoutSpaces
+function isStrPalindrome(string) {
+    const separator = '';
+    const normalizedStr = string.toLowerCase().replace(/[^a-z0-9]/g, '');
+    console.log(normalizedStr);
+    const reversedStr = normalizedStr
         .split(separator)
         .reverse()
         .join(separator);
-    return stringWithoutSpaces === stringWithoutSpacesReversed;
+    console.log(reversedStr);
+    return normalizedStr === reversedStr;
 }
 
-console.log(isStrPalindrome(string, separator));
+console.log(isStrPalindrome(string));
