4ee24515556f4af16797c3cea7ed7da4529ae41c
Borys Ziukov <ziukoff1985@gmail.com>
2025-08-29 11:01:23 +0300

    Improve regExp

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 const string = 'Are we not drawn onward, we few, drawn onward to new era?';
 
 function isStrPalindrome(string) {
-    const normalizedStr = string.toLowerCase().replace(/[^a-z0-9]/g, '');
+    const normalizedStr = string.toLowerCase().replace(/[\s\W]/g, '');
     const reversedStr = normalizedStr.split('').reverse().join('');
     return normalizedStr === reversedStr;
 }
@@ -11,3 +11,6 @@ function isStrPalindrome(string) {
 isStrPalindrome(string)
     ? console.log(`'${string}': is palindrome`)
     : console.log(`'${string}': is not palindrome`);
+
+// * регулярний вираз (простий) з метасимволами: /[\s\W]/g - видалити пробіли та небуквенні символи
+// * регулярний вираз (альтернативний): /[^a-z0-9]/g - видалити всі символи, крім букв та цифр
